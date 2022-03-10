b7536830fc063d0d5f985c7d2d4370dce5743cd3
Hadar Rosenwald <hadar@sunbit.com>
2022-03-10 21:06:55 +0200

    new_ye_lyrics_source

--- raw_data/txt_lyric_to_csv.py
+++ raw_data/txt_lyric_to_csv.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
-if __name__ == '__main__':
-    with open('avicii_lyrics.txt', 'r') as f:
+from config import lyrics_dir_path
+
+
+def raw_txt_to_csv(artist: str):
+    with open(f'{artist}_lyrics.txt', 'r') as f:
         output = f.read()
     song_df = pd.DataFrame([song for song in output.split('\n\n\n')])
     song_df = song_df.rename(columns={0: 'Lyrics'})
-    song_df['Artist'] = 'Avicii'
-    song_df.to_csv('../Data/Avicii.csv', index=False)
+    song_df['Artist'] = artist.capitalize()
+    song_df.to_csv(f'../{lyrics_dir_path}{artist.capitalize()}.csv', index=False)
+
+
+if __name__ == '__main__':
+    raw_txt_to_csv('avicii')
+    raw_txt_to_csv('kanye')
