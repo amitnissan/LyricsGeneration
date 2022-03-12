c001ef3c2337aff35b5d433169ce7384f2bf5660
amitn <amitnissan20@gmail.com>
2022-03-12 14:12:08 +0200

    Config changes + Evaluation mechanism added

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ output_dir = 'output'
 device = 'cuda'
 
 # fine tune hyperparameters
-num_epoch = 3
+num_epoch = 40
 batch_size = 2
 
 # generating lyrics hyperparameters
-temperature = 0.7
+temperature = 0.4
 k = 20
 p = 0.9
-repetition_penalty = 1.0
-num_return_sequences = 1  # num generated songs
-length = 500
+repetition_penalty = 3.0
+num_return_sequences = 20  # num generated songs
+length = 600
 stop_token = '|EndOfText|'
